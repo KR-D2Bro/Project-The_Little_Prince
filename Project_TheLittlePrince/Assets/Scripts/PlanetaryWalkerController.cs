51574624bf3740fd140d8f2ad7ec43325cb76d5f
hs986 <94986211+hs986@users.noreply.github.com>
2023-10-19 23:40:58 +0900

    TLP move
    TLP move
    planet TLP 충돌문제 해결

---
+++ Project_TheLittlePrince/Assets/Scripts/PlanetaryWalkerController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class PlanetaryWalkerController : MonoBehaviour
+{
+    // 플레이어의 이동 속도
+    public float movementspeed = 10.0f;
+
+    // 점프 가능 여부
+    public bool canJump = true;
+
+    // 점프 높이
+    public float jumpHeight = 5.0f;
+
+    // 플레이어 카메라
+    public Camera playerCamera;
+
+    // 카메라 회전 속도
+    public float CameraSpeed = 2.0f;
+
+    // 카메라 X 회전 한계
+    public float CameraXLimit = 30.0f;
+
+    bool grounded = false;
+
+    Rigidbody Player;
+
+    Vector2 rotation = Vector2.zero;
+
+    float VelocityChangeLimit = 10.0f;
+
+    void Awake()
+    {
+        // Rigidbody 컴포넌트 가져오기
+        Player = GetComponent<Rigidbody>();
+
+        // Rigidbody 설정 초기화
+        Player.freezeRotation = true;
+        Player.useGravity = false;
+        Player.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+
+        // 초기 회전 값 설정
+        //rotation.y= 0f;
+        rotation.y = transform.eulerAngles.y;
+        //transform.rotation = Quaternion.identity;
+
+        // 커서 숨기고 고정
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            // 플레이어 이동 속도 계산
+            Vector3 forwardDir = Vector3.Cross(transform.up, -playerCamera.transform.right).normalized;
+            Vector3 rightdirection = Vector3.Cross(transform.up, playerCamera.transform.forward).normalized;
+            Vector3 targetVelocity = (forwardDir * Input.GetAxis("Vertical") + rightdirection * Input.GetAxis("Horizontal")) * movementspeed;
+
+            // 현재 속도 및 변화량 계산
+            Vector3 velocity = transform.InverseTransformDirection(Player.velocity);
+            velocity.y = 0;
+            velocity = transform.TransformDirection(velocity);
+            Vector3 velocityChange = transform.InverseTransformDirection(targetVelocity - velocity);
+            velocityChange.x = Mathf.Clamp(velocityChange.x, -VelocityChangeLimit, VelocityChangeLimit);
+            velocityChange.z = Mathf.Clamp(velocityChange.z, -VelocityChangeLimit, VelocityChangeLimit);
+            velocityChange.y = 0;
+            velocityChange = transform.TransformDirection(velocityChange);
+
+            // 속도 변경 적용
+            Player.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            // 점프 처리
+            if (Input.GetButton("Jump") && canJump)
+            {
+                Player.AddForce(transform.up * jumpHeight, ForceMode.VelocityChange);
+            }
+        }
+
+        grounded = false;
+
+        // 카메라 및 플레이어 회전 입력 처리
+        rotation.x += -Input.GetAxis("Mouse Y") * CameraSpeed;
+        rotation.x = Mathf.Clamp(rotation.x, -CameraXLimit, CameraXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+        Quaternion localRotation = Quaternion.Euler(0f, Input.GetAxis("Mouse X") * CameraSpeed, 0f);
+        transform.rotation = transform.rotation * localRotation;
+    }
+
+    void OnCollisionStay()
+    {
+        // 플레이어가 바닥에 닿았는지 확인
+        grounded = true;
+    }
+}
