7dd248733a1857dab2eab139992933d7aba6096d
hs986 <94986211+hs986@users.noreply.github.com>
2023-11-20 20:10:29 +0900

    sprout Objdata
    100으로 설정

--- Project_TheLittlePrince/Assets/Scripts/TALK/GameManager.cs
+++ Project_TheLittlePrince/Assets/Scripts/TALK/GameManager.cs
@@ -13,22 +13,14 @@ public class GameManager : MonoBehaviour
     public int talkIndex;
     public void Action(RaycastHit hitInfo)
     {
-        if (isAction)
-        {
-            isAction = false;
-        }
-        else
-        {
-            isAction = true;  //액션이 사실이면         
+           
             // hitInfo에서 필요한 정보 추출
-            GameObject hitObject = hitInfo.collider.gameObject;
+        GameObject hitObject = hitInfo.collider.gameObject;
 
             // scanObject에 할당
-            scanObject = hitObject;
-            objData objData = scanObject.GetComponent<objData>();
-            Talk(objData.id, objData.isNpc);
-
-        }
+        scanObject = hitObject;
+        objData objData = scanObject.GetComponent<objData>();
+        Talk(objData.id, objData.isNpc);
         talkPanel.SetActive(isAction); //창을 띄우고
 
     }
@@ -36,15 +28,21 @@ public class GameManager : MonoBehaviour
     void Talk(int id, bool isNpc)
     {
         string talkData = talkManager.GetTalk(id, talkIndex);
-
+        if (talkData == null) {
+            isAction = false; //대화가 끝나면 액션을 비활성화 하고 대화 순서을 0로 만들어 오류가 안나게
+            talkIndex = 0;
+            return;
+        }
         if (isNpc)
         {
             talkText.text = talkData;
         }
         else
         {
-            talkText.text = talkData;
+            talkText.text = talkData;// 초상화를 위해 나눔 npc인 경우와 아닌 경우
         }
+        isAction = true;
+        talkIndex++;
     }
 
 
--- Project_TheLittlePrince/Assets/Scripts/TALK/TalkManager.cs
+++ Project_TheLittlePrince/Assets/Scripts/TALK/TalkManager.cs
@@ -15,11 +15,14 @@ public class TalkManager : MonoBehaviour
     // Update is called once per frame
     void GenerateData()
     {
-        talkData.Add(1000, new string[] { "어린왕자야" });
+        talkData.Add(1000, new string[] { "어린왕자야, 큰일났어","바오밥 나무가 자란다면 별에 구멍이 날꺼야" });
 
     }
     public string GetTalk(int id, int talkIndex)
     {
-        return talkData[id][talkIndex];
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
     }
 }
