8968802abbfb451c07c0211d52dc40d5c66da015
hs986 <94986211+hs986@users.noreply.github.com>
2023-12-10 01:41:30 +0900

    ddd

--- Project_TheLittlePrince/Assets/Scripts/PlanetaryWalkerController.cs
+++ Project_TheLittlePrince/Assets/Scripts/PlanetaryWalkerController.cs
@@ -91,7 +91,6 @@ public class PlanetaryWalkerController : MonoBehaviour
                 IsWalking=false;
                 anim.SetBool("IsWalk",false);
             }
-            
 
             // 속도 변경 적용
             Player.AddForce(velocityChange, ForceMode.VelocityChange);
